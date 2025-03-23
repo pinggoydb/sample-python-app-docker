4f4e708530074b6fc984715907bfbe980e5361d3
Dante Pinggoy <dante.pinggoy@waterfield.com>
2025-03-23 18:41:29 +0800

    flask app

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import time
 
 import redis
-from flask import Flask
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+app.secret_key = "&uo+!8&luu7vq4k)w+wb8y2&8*9q#9(*(+d3c2)8p#0!6(yj+$"
 cache = redis.Redis(host='redis', port=6379)
 
 def get_hit_count():
@@ -20,4 +21,14 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return f'Hello World! I have been seen {count} times.\n'
+    return f'<h1>Hello World! I have been seen {count} times.</h1>\n'
+
+@app.route('/hello')
+def index():
+    flash("what's your name?")
+    return render_template("index.html")
+
+@app.route('/greet', methods=["POST", "GET"])
+def greet():
+    flash("Hi " +str(request.form['name_input']) + ", great to see you!")
+    return render_template("index.html")
